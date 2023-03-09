d51a7130b900dcc14a1bba408c91fef51c253b50
owanray11 <petersr2919@my.uwstout.edu>
2023-03-08 20:27:45 -0600

    Added scene management

--- DogGone/Assets/Scripts/RowanScripts/Enemy/EnemyData.cs
+++ DogGone/Assets/Scripts/RowanScripts/Enemy/EnemyData.cs
@@ -13,11 +13,15 @@ public class EnemyData : MonoBehaviour
     [SerializeField] private int maxHealth;
     [Tooltip("Keep this at zero")] [SerializeField] private int currentHealth;
 
+    private LevelChange gameManager;
     private int damage = 5; public int getDamage() { return damage; }
 
     private void Start()
     {
         currentHealth = maxHealth;
+        gameManager = FindObjectOfType<LevelChange>();
+        if (gameManager == null) { Debug.LogError("There is no level change on the game manager in the current scene"); }
+        else { gameManager.IncrementEnemies(); }
     }
 
     public void takeDamage(int damage)
@@ -26,13 +30,14 @@ public class EnemyData : MonoBehaviour
         Debug.Log($"enemy took {maxHealth - currentHealth} damage");
         if (currentHealth <= 0)
         {
-            Debug.Log("Enemy Died");
-            Destroy(gameObject);
+            die();
         }
     }
 
     public void die()
     {
-
+        Debug.Log("Enemy Died");
+        gameManager.DecrementEnemies();
+        Destroy(gameObject);
     }
 }
--- DogGone/Assets/Scripts/RowanScripts/Player/PlayerData.cs
+++ DogGone/Assets/Scripts/RowanScripts/Player/PlayerData.cs
@@ -9,9 +9,13 @@ public class PlayerData : MonoBehaviour
     [SerializeField] private int maxHealth;
     [SerializeField] private int currentHealth;
 
+    private LevelChange gameManager;
+
     public void Start()
     {
         currentHealth = maxHealth;
+        gameManager = FindObjectOfType<LevelChange>();
+        if (gameManager == null) { Debug.LogError("There is no level change on the game manager in the current scene"); }
     }
 
     public void takeDamage(int damage)
@@ -22,5 +26,11 @@ public class PlayerData : MonoBehaviour
         {
             Debug.Log("Player has died");
         }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player has died");
+        gameManager.Win();
     }
 }
