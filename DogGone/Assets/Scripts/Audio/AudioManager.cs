5b1406c8bc32a7822e48eecf4729b727f94b2c86
PapayamayaToo <awpersonal03@gmail.com>
2023-03-08 19:57:30 -0600

    music

--- DogGone/Assets/Scripts/Audio/AudioManager.cs
+++ DogGone/Assets/Scripts/Audio/AudioManager.cs
@@ -31,6 +31,8 @@ public class AudioManager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
+
+        Play("MonkeysSpinningMonkeys");
     }
 
     public void Play(string name)
