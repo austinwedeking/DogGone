cfd2c1b6e1f03435c5f8226b1565e33b43aca06c
WyattTreinen <wyatttreinen@gmail.com>
2023-03-21 17:05:23 -0500

    Test to see if this break for rowan too
    Everything is breaking lol

---
+++ DogGone/Assets/Scripts/RowanScripts/Enemy/CostomAIControllers/SlamAI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlamAI : BaseAI
+{
+    private Rigidbody2D AIRigid;
+    private Transform playerTransform;
+    public override void Attack() //Likely the slam attack
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Chase() //Likely chhase player
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ChaseWithinRange() //Also a chase player
+    {
+        throw new System.NotImplementedException();
+    }
+    public override void Wander() //Roaming code
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void FaceLeft(bool faceRight) //Something something animations
+    {
+        throw new System.NotImplementedException();
+    }
+}
