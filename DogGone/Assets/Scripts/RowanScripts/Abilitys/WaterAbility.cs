0d88907685276976d6c3b8d0f3766ee9f8ce1da2
owanray11 <petersr2919@my.uwstout.edu>
2023-04-12 19:58:37 -0500

    Added water ability

---
+++ DogGone/Assets/Scripts/RowanScripts/Abilitys/WaterAbility.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterAbility : BaseAbility
+{
+    [Header("Circle Collider Options")]
+    [SerializeField] private float radius;
+    [SerializeField] private float force;
+
+    private GameObject player;
+
+    private void Settup()
+    {
+        player = FindObjectOfType<PlayerData>().gameObject;
+    }
+
+    public override void Cast()
+    {
+        Settup();
+        Collider2D[] collision = Physics2D.OverlapCircleAll(player.transform.position, radius, enemyLayer);
+        EnemyData temp;
+        for (int i = 0; i < collision.Length; ++i)
+        {
+            temp = null;
+            temp = collision[i].GetComponent<EnemyData>();
+            if (temp != null)
+            {
+                temp.takeDamage(damage, 0f, force);
+            }
+        }
+    }
+
+    public override void Upgrade()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
